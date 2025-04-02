21432ccee5d05e881f621727427d8bcd4ea82da1
milanmstc <milan.patel@outamation.com>
2025-04-02 17:51:08 +0530

    show notification

--- src/MahantInv.Web/wwwroot/Notification/mynotification.js
+++ src/MahantInv.Web/wwwroot/Notification/mynotification.js
@@ -1,7 +1,53 @@
 ﻿
 let down = false;
 let notificationIds = [];
-let pendingNotificationCount = 0;
+let notificationGridAPI;
+
+var notificationGridAPIOptions = {
+
+    // define grid columns
+    columnDefs: [
+        {
+            headerName: 'title', field: 'title', filter: 'agTextColumnFilter', headerTooltip: 'Title'
+        },
+        {
+            headerName: 'Message', field: 'message', filter: 'agTextColumnFilter', headerTooltip: 'Message'
+        },
+        {
+            headerName: 'Status', field: 'status', filter: 'agTextColumnFilter', headerTooltip: 'Status'
+        },
+        //{
+        //    headerName: '', field: 'id', headerTooltip: 'Action', pinned: 'right', suppressSizeToFit: true,
+        //    cellRenderer: 'actionCellRenderer',
+        //}
+    ],
+    defaultColDef: {
+        editable: false,
+        sortable: true,
+        resizable: true,
+        flex: 1,
+        minWidth: 50,
+        wrapText: true,
+        autoHeight: true,
+        floatingFilter: false,
+    },
+    animateRows: true,
+    pagination: true,
+    paginationPageSize: 200,
+    getRowId: params => {
+        return params.data.id;
+    },
+    onGridReady: function (params) {
+        notificationGridAPI.sizeColumnsToFit();
+    },
+    overlayLoadingTemplate:
+        '<span class="ag-overlay-loading-center">Please wait while your notifications are loading</span>',
+    overlayNoRowsTemplate:
+        `<div class="text-center">
+                <h5 class="text-center"><b>Notification(s) will appear here.</b></h5>
+            </div>`
+};
+
 class MyNotification {
 
     static NotificationMessageTemplate() {
@@ -13,31 +59,27 @@ class MyNotification {
         fetch(baseUrl + 'api/notification/pendingornotified')
             .then((response) => response.json())
             .then(data => {
-                $('#NotificationMessages').empty();
-                if (data.pendingNotificationCount > 0) {
-                    $('#NotificationPendingCount').show();
-                    $('#NotificationPendingCount').html(data.pendingNotificationCount);
-                    pendingNotificationCount = data.pendingNotificationCount;
-
-                } else {
-                    $('#NotificationPendingCount').hide();
-                    pendingNotificationCount = 0;
-
-                }
-                if (data.myNotifications.length == 0) {
-                    $('#NotificationMessages').html('<h2>Notifications</h2><h6>Notifications will be appear here</h6>');
-                }
-                else {
-                    $('#NotificationMessages').html('<h2>Notifications</h2>');
-                    let template = MyNotification.NotificationMessageTemplate();
+                notificationGridAPI.setGridOption("rowData", data.myNotifications);
+                if (data.myNotifications.length != 0) {
                     $.each(data.myNotifications, function (i, v) {
-                        let message = template.supplant(v);
-                        $('#NotificationMessages').append(message);
                         if (v.status === 'Pending') {
                             notificationIds.push(v.id);
                         }
                     });
                 }
+                //$('#NotificationMessages').empty();
+                if (data.pendingNotificationCount > 0) {
+                    $('#NotificationPendingCount').show();
+                    $('#NotificationPendingCount').html(data.pendingNotificationCount);
+                    MyNotification.MarkAsNotified();
+                }
+
+                //if (data.myNotifications.length == 0) {
+                //    $('#NotificationMessages').html('<h2>Notifications</h2><h6>Notifications will be appear here</h6>');
+                //}
+                //else {
+                //    $('#NotificationMessages').html('<h2>Notifications</h2>');
+                //    let template = MyNotification.NotificationMessageTemplate();
 
             })
             .catch(error => {
@@ -82,27 +124,42 @@ class MyNotification {
     }
 
     static async ViewNotificationModal(mthis) {
-        return false;
-        if (down) {
-            $('#NotificationMessages').css('height', '0px');
-            $('#NotificationMessages').css('opacity', '0');
-            down = false;
-        } else {
-            $('#NotificationMessages').css('height', 'auto');
-            $('#NotificationMessages').css('opacity', '1');
-            down = true;
-        }
-        if (pendingNotificationCount > 0) {
-            MyNotification.MarkAsNotified();
-        }
-
+        $('#NotificationModal').modal('show');
+        //if (down) {
+        //    $('#NotificationMessages').css('height', '0px');
+        //    $('#NotificationMessages').css('opacity', '0');
+        //    down = false;
+        //} else {
+        //    $('#NotificationMessages').css('height', 'auto');
+        //    $('#NotificationMessages').css('opacity', '1');
+        //    down = true;
+        //}
     }
 
     static init() {
+        MyNotification.ApplyAGGrid();
         MyNotification.GetPendingORNotifiedNotifications();
     }
 
-
+    static ApplyAGGrid() {
+        var gridDiv = document.querySelector('#notificationgrid');
+        //var state = localStorage.getItem("r447774316684a1d9074esdscddc7a02s23");
+        //if (state) {
+        //    notificationGridAPIOptions.initialState = JSON.parse(state);
+        //}
+        notificationGridAPI = new agGrid.createGrid(gridDiv, notificationGridAPIOptions);
+        notificationGridAPI.setGridOption("theme", agGrid.themeQuartz
+            .withParams(
+                {
+                    backgroundColor: "#1e2838",
+                    foregroundColor: "#FFFFFFCC",
+                    browserColorScheme: "dark",
+                },
+                "dark-red",
+            ));
+        document.body.dataset.agThemeMode = "dark-red";
+        notificationGridAPI.setGridOption("rowData", []);
+    }
 }
 
 jQuery(document).ready(function () {
